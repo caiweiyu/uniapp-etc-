164b471606bcc65dbc89f9d29edd78118aa0dfb4
caiweiyu <1172822784@qq.com>
2021-09-15 15:46:58 +0800

    add

--- packageA/pages/ytk/bill_all/components/monfree-timer.vue
+++ packageA/pages/ytk/bill_all/components/monfree-timer.vue
@@ -1,14 +1,16 @@
 <template>
     <view>
         <!--本月消费、同行次数-->
-        <view class="box">
-            <view class="box1">
-              ¥<text class="box1_text">{{details.sum}}</text>
-              <view class="box1_view">本月消费</view>
-            </view>
-            <view class="box1">
-              <text class="box1_text">{{details.num}}</text>
-              <view class="box1_view">通行次数</view>
+        <view class="container">
+            <view class="box">
+              <view class="box1">
+                ¥<text class="box1_text">{{details.sum}}</text>
+                <view class="box1_view">本月消费</view>
+              </view>
+              <view class="box1">
+                <text class="box1_text">{{details.num}}</text>
+                <view class="box1_view">通行次数</view>
+              </view>
             </view>
         </view>
         <!--可滑动 自动滚区-->
@@ -44,23 +46,34 @@ export default {
 </script>
 
 <style scoped lang="scss">
-  .box{
-    display: flex;
-    justify-content: space-around;
-    height: 160rpx;
-    align-items: center;
-    .box1{
-      width: 120rpx;
-      text-align: center;
-      &_text{
-        color: #222222;
-        font-size: 48rpx;
+  .container{
+      width: 750rpx;
+      height: 207rpx;
+      padding-top: 30rpx;
+      background-color: #F6F6F6;
+      .box{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin:auto;
+        width: 690rpx;
+        height: 150rpx;
+        //@errorpadding: 30rpx 30rpx 20rpx 30rpx;
+        border-radius: 16rpx;
+        background-color: #FFFFFF;
+        .box1{
+            width: 120rpx;
+            text-align: center;
+            &_text{
+              color: #222222;
+              font-size: 48rpx;
+            }
+            &_view{
+              color: #999999;
+              font-size: 24rpx;
+            }
+        }
       }
-      &_view{
-        color: #999999;
-        font-size: 24rpx;
-      }
-    }
   }
   .swiper{
     position: relative;
